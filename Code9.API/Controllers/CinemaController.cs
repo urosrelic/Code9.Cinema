46649f5076ff7df96e009acacda86d9ec394ef94
urosrelic <urosrelicc@gmail.com>
2024-05-11 02:17:57 -0700

    feat: add CinemaController

---
+++ Code9.API/Controllers/CinemaController.cs
@@ -0,0 +1,30 @@
+﻿using Code9.Domain.Models;
+using Code9.Domain.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Code9.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CinemaController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CinemaController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("get-all-cinemas")]
+        public async Task<ActionResult<List<Cinema>>> GetAllCinema()
+        {
+            var query = new GetAllCinemaQuery();
+            var cinemas = await _mediator.Send(query);
+
+            return Ok(cinemas);
+        }
+    }
+}
