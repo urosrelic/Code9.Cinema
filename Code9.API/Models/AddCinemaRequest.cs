c85f8c8f3068bf55810297f3c2dc1aff74f96b4a
urosrelic <urosrelicc@gmail.com>
2024-05-11 03:08:02 -0700

    feat: create a cinema command request DTO

---
+++ Code9.API/Models/AddCinemaRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Code9.API.Models
+{
+    public class AddCinemaRequest
+    {
+        public string? Name { get; set; }
+        public string? City { get; set; }
+        public string? Street { get; set; }
+        public int NumberOfAuditoriums { get; set; }
+    }
+}
