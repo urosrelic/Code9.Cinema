d50272bf120500792f9957b0dab7db35f96ae3b7
urosrelic <urosrelicc@gmail.com>
2024-05-11 01:13:17 -0700

    feat: create AddCinema and UpdateCinema functions

--- Code9.Domain/Interfaces/ICinemaRepository.cs
+++ Code9.Domain/Interfaces/ICinemaRepository.cs
@@ -5,5 +5,9 @@ namespace Code9.Domain.Interfaces
     public interface ICinemaRepository
     {
         public Task<List<Cinema>> GetAllCinemas();
+        public Task addCinema(Cinema cinema);
+        public Task updateCinema(Cinema cinema);
     }
+
+    
 }
